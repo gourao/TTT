8efbc1632e29a3d9a2ed21ef8deff7a1183a0fb0
Gou Rao <goutham.rao@gmail.com>
2020-12-30 19:53:43 -0800

    optimal now for a second player

--- ticTacToe.py
+++ ticTacToe.py
@@ -22,51 +22,50 @@ for key in masterBoard:
     # generates this list: ['7', '8', '9', '4', '5', '6', '1', '2', '3'] BUT WHY?
     boardKeys.append(key)
 
-print(boardKeys)
 '''We will have to print the updated board after every move in the game and
     thus we will make a function in which we'll define the printBoard function
     so that we can easily print the board everytime by calling this function.'''
 
 def printBoard(board):
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
-    print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
-    print('-+-+-')
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print(board['7'] + '|' + board['8'] + '|' + board['9'] + "      7|8|9")
+    print('-+-+-      -+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'] + "      4|5|6")
+    print('-+-+-      -+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'] + "      1|2|3")
 
 def winLogic(board, turn):
     value = False
     if board['7'] == board['8'] == board['9'] != ' ': # across the top # TURN WINNING LOGIC INTO A SEPARATE FUNCTION
         printBoard(board)
-        print("**** " + turn + " will win. ****")
+        print("**** " + turn + " will win (7 - 8 - 9). ****")
         value = True
     elif board['4'] == board['5'] == board['6'] != ' ': # across the middle
         printBoard(board)
-        print("**** " + turn + " will win. ****")
+        print("**** " + turn + " will win (4 - 5 - 6). ****")
         value = True
     elif board['1'] == board['2'] == board['3'] != ' ': # across the bottom
         printBoard(board)
-        print("**** " + turn + " will win. ****")
+        print("**** " + turn + " will win (1 - 2 - 3). ****")
         value = True
     elif board['1'] == board['4'] == board['7'] != ' ': # down the left side
         printBoard(board)
-        print("**** " + turn + " will win. ****")
+        print("**** " + turn + " will win (1 - 4 - 7). ****")
         value = True
     elif board['2'] == board['5'] == board['8'] != ' ': # down the middle
         printBoard(board)
-        print("**** " + turn + " will win. ****")
+        print("**** " + turn + " will win (2 - 5 - 8). ****")
         value = True
     elif board['3'] == board['6'] == board['9'] != ' ': # down the right side
         printBoard(board)
-        print("**** " + turn + " will win. ****")
+        print("**** " + turn + " will win (3 - 6 - 9). ****")
         value = True
     elif board['7'] == board['5'] == board['3'] != ' ': # diagonal
         printBoard(board)
-        print("**** " + turn + " will win. ****")
+        print("**** " + turn + " will win (7 - 5 - 3). ****")
         value = True
     elif board['1'] == board['5'] == board['9'] != ' ': # diagonal
         printBoard(board)
-        print("**** " + turn + " will win. ****")
+        print("**** " + turn + " will win (1 - 5 - 9). ****")
         value = True
     return value
 
@@ -132,8 +131,9 @@ def simulate(board, currentTurn):
 # Now we'll write the main function which has all the gameplay functionality.
 def game():
     turn = 'X'
-    count = 0
+    round = 0
     for i in range(10): #iterate 9 times
+        print("Round ", round, "... Board Layout:")
         printBoard(masterBoard)
 
         if turn == 'O': #if O's turn
@@ -149,8 +149,8 @@ def game():
             # open and we don't have a winner, pick that spot since the
             # computer is going second.  This is purely a non algorithmic
             # optimization for Tic Tac Toe.
-            if count > 0 and masterBoard["5"] == ' ':
-                print("Computer taking open mid position")
+            if round > 0 and masterBoard["5"] == ' ':
+                print("*** BOOK MOVE: Computer taking open mid position")
                 move = "5"
         else:
             print("It's your turn, " + turn + ". Move to which place?")
@@ -158,14 +158,14 @@ def game():
 
         if masterBoard[move] == ' ': #if dict[key] = empty, put x in place
             masterBoard[move] = turn
-            count += 1
+            round += 1
         else:
             print("That place is already filled. Move to which place?")
             continue
 
         # Now we will check if player X or O has won,for every move after 5 moves.
-        # Because the min # after which someone can win is in 5 counted moves from both.
-        if count >= 5:
+        # Because the min # after which someone can win is in 5 rounded moves from both.
+        if round >= 5:
             if winLogic(masterBoard, turn):
                 print("Game Over. ", turn, " Won!")
                 sys.exit()
